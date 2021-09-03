6f707f60f538413ab68978520cdb5e44b67f4f7a
DaanS <Daan.Schaer@Gmail.com>
2021-09-03 13:40:20 +0200

    init

---
+++ desktop/src/com/university/maastricht/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.university.maastricht.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.university.maastricht.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
