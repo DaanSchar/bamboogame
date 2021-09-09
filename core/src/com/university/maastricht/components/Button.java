0c79d891abfcd24faecbbd22c6bbda113977001e
DaanSchar <Daan.Schaer@Gmail.com>
2021-09-09 02:43:07 +0200

    added fun animation

--- core/src/com/university/maastricht/components/Button.java
+++ core/src/com/university/maastricht/components/Button.java
@@ -1,11 +1,13 @@
 package com.university.maastricht.components;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateByAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
 import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
-import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -25,7 +27,11 @@ public class Button extends Image {
     private ActorGestureListener click()  {
         return new ActorGestureListener(){
             public void touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                addAction(scaleAction(1.13f,0.03f));
+                if (button == 0) {
+                    addAction(scaleAction(1.13f, 0.03f));
+                    addAction(rotateByAction(-25, 0.2f));
+                }
+
             }
         };
     }
@@ -35,16 +41,21 @@ public class Button extends Image {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 addAction(scaleAction(1.07f,0.1f));
+                addAction(rotateByAction(180, 0.4f));
             }
 
             public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
-                if (pointer == -1)
-                    addAction(scaleAction(1.07f,0.05f));
+                if (pointer == -1) {
+                    addAction(scaleAction(1.07f, 0.05f));
+                    addAction(rotateByAction(-30, 0.3f));
+                }
             }
 
             public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-                if (pointer == -1)
+                if (pointer == -1) {
                     addAction(scaleAction(1f, 0.15f));
+                    addAction(rotateByAction(30, 0.3f));
+                }
             }
         };
     }
@@ -56,6 +67,23 @@ public class Button extends Image {
         ScaleToAction action = new ScaleToAction();
         action.setScale(scale);
         action.setDuration(time);
+        return action;
+    }
+
+    private Action rotateToAction(float rotation, float time) {
+        RotateToAction action = new RotateToAction();
+        action.setRotation(rotation);
+        action.setDuration(time);
+        action.setInterpolation(Interpolation.exp5);
+        return action;
+    }
+
+
+    private Action rotateByAction(float rotation, float time) {
+        RotateByAction action = new RotateByAction();
+        action.setAmount(rotation);
+        action.setDuration(time);
+        action.setInterpolation(Interpolation.exp5);
         return action;
     }
 
