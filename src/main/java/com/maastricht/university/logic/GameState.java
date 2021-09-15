31d1e42ce2d8a8f2fa59b6843b3977b52913e5eb
DaanSchar <Daan.Schaer@Gmail.com>
2021-09-15 14:49:27 +0200

    revert back to javafx. probably easiest choice

---
+++ src/main/java/com/maastricht/university/logic/GameState.java
@@ -0,0 +1,55 @@
+package com.maastricht.university.logic;
+
+public class GameState {
+
+    private int numberOfGroupsBlue;
+    private int numberOfGroupsRed;
+    private boolean redsTurn;
+    private Tile[][][] board;
+
+    public GameState() {
+        this.numberOfGroupsBlue = 0;
+        this.numberOfGroupsRed = 0;
+        this.redsTurn = true;
+        this.board = new Tile[9][9][9];
+        for(int i=0; i<7; i++) {
+            for(int j=0; j<7; j++) {
+                for(int k=0; k<7; k++) {
+
+                }
+            }
+        }
+    }
+
+    public boolean[][][] getLegalMovesBlue() {
+        return new boolean[9][9][9];
+    }
+
+    public boolean[][][] getLegalMovesRed() {
+        return new boolean[9][9][9];
+    }
+
+    public void moveBlue() {
+        if(!redsTurn) {
+            //update board
+            //recount number of groups for Blue
+            //update legal moves
+        }
+    }
+
+    public void moveRed() {
+        if(redsTurn) {
+            //update board
+            //recount number of groups for Red
+            //update legal moves
+        }
+    }
+
+    public int getNumberOfGroupsBlue() {
+        return numberOfGroupsBlue;
+    }
+
+    public int getNumberOfGroupsRed() {
+        return numberOfGroupsRed;
+    }
+}
---
+++ src/main/java/com/maastricht/university/logic/Tile.java
@@ -0,0 +1,26 @@
+package com.maastricht.university.logic;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Tile {
+    private int id;
+    private int groupNumber;
+    private int groupSize;
+
+    private List<Tile> groupMembers = new LinkedList<Tile>();
+    private List<Tile> neighbours = new LinkedList<Tile>();
+
+    private static int count;
+
+    public Tile(){
+        this.id = count;
+        count++;
+        groupNumber = 0;
+        groupSize = 0;
+    }
+
+    public void SetNeighbours(Tile neighbour) {
+
+    }
+}
