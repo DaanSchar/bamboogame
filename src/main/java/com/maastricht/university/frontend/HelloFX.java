6f08fefd8c2ca2d0efebdd095de6dfa58127ee39
Fabian Brock <fabian.brock03@gmail.com>
2021-09-15 21:40:49 +0200

    Moved Loop etc. from Main to TileMap.java

--- src/main/java/com/maastricht/university/frontend/HelloFX.java
+++ src/main/java/com/maastricht/university/frontend/HelloFX.java
@@ -11,23 +11,10 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        Pane playground = new Pane();
-        //Commented this out for now
-        //Tile a = new Tile(200, 300, 100, 0);
-        //Tile b = new Tile(100, 100, 200, 1);
-        //playground.getChildren().add(a.getButton());
-        //playground.getChildren().add(b.getButton());
 
         TileMap tilemap = new TileMap(0,0,30);
-        int dim = tilemap.getDimension();
-        Tile[][] map = tilemap.getTileMap();
+        Pane playground = tilemap.getTileMapPane();
 
-        for(int i = 0; i<dim;i++) {
-            for(int j = 0; j<dim;j++) {
-                if(map[i][j] != null)
-                    playground.getChildren().add(map[i][j].getButton());
-            }
-        }
         Scene scene = new Scene(playground, 640, 480);
         stage.setScene(scene);
         stage.show();
