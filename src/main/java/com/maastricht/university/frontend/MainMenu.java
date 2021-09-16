ef3245084251ec7fe2a80d1af244b2807da82019
michellefilipaa <m.lear@student.maastrichtuniversity.nl>
2021-09-16 17:33:16 +0200

    added exit feature rules button opens another window

--- src/main/java/com/maastricht/university/frontend/MainMenu.java
+++ src/main/java/com/maastricht/university/frontend/MainMenu.java
@@ -1,12 +1,17 @@
 package com.maastricht.university.frontend;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class MainMenu extends Application {
 
@@ -29,7 +34,7 @@ public class MainMenu extends Application {
 
     @Override
     public void start(Stage stage) {
-        Pane r = new Pane();
+        BorderPane r = new BorderPane();
         Scene scene = new Scene(r, width, height);
 
         //set background picture
@@ -51,33 +56,64 @@ public class MainMenu extends Application {
         r.getChildren().add(title);
 
         //create buttons and add the design
-        HoverableButton human = new HoverableButton(width / 2, height / 2, 20, 20);
+        HoverableButton human = new HoverableButton(width / 2, height / 3, 20, 20);
         humanView.setFitHeight(100);
         humanView.setFitWidth(270);
         human.setGraphic(humanView);
 
-        HoverableButton computer = new HoverableButton(width / 2, height / 3, 20, 20);
+        human.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("This goes into the game :)");
+            }
+        });
+
+        HoverableButton computer = new HoverableButton(width / 2, height / 2, 20, 20);
         pcView.setFitHeight(100);
         pcView.setFitWidth(270);
         computer.setGraphic(pcView);
+
+        computer.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("Opens game");
+            }
+        });
 
         HoverableButton exit = new HoverableButton(200, height - height / 4, 20, 20);
         exitView.setFitHeight(90);
         exitView.setFitWidth(175);
         exit.setGraphic(exitView);
 
+        //if the exit button is clicked, it will close the program
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        });
+
         HoverableButton rules = new HoverableButton(width - width / 6, height / 6, 20, 20);
         rulesView.setFitHeight(90);
         rulesView.setFitWidth(175);
         rules.setGraphic(rulesView);
 
+        rules.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                Stage rulesWindow = new Stage();
+                Pane r = new Pane();
+                Scene sc = new Scene(r,width/2.5,height/2.5);
+                rulesWindow.setScene(sc);
+                
+                rulesWindow.show();
+
+                System.out.println("Opens tutorial");
+            }
+        });
 
         //add to scene
         r.getChildren().add(human);
         r.getChildren().add(computer);
         r.getChildren().add(exit);
         r.getChildren().add(rules);
-
 
         stage.show();
     }
