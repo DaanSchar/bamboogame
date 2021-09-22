5797ccbf4f2014e2c0d86003f1df13e5061dea5f
DaanS <Daan.Schaer@Gmail.com>
2021-09-22 12:51:46 +0200

    quick setup for hexagon class

---
+++ src/main/java/com/maastricht/university/logic/Hexagon.java
@@ -0,0 +1,28 @@
+package com.maastricht.university.logic;
+
+public class Hexagon<T> implements IHexagon<T>{
+    @Override
+    public T get(int x, int y, int z) {
+        return null;
+    }
+
+    @Override
+    public T insert(int x, int y, int z) {
+        return null;
+    }
+
+    @Override
+    public T remove(int x, int y, int z) {
+        return null;
+    }
+
+    @Override
+    public IHexagon<T> clone() {
+        return null;
+    }
+
+    @Override
+    public boolean contains(T entity) {
+        return false;
+    }
+}
