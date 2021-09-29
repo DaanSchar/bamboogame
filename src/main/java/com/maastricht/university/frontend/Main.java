bf72ba903657ecb565bd834ee1f9451e3f698234
michellefilipaa <m.lear@student.maastrichtuniversity.nl>
2021-09-29 17:18:31 +0200

    public variables to private delete commented code

--- src/main/java/com/maastricht/university/frontend/Main.java
+++ src/main/java/com/maastricht/university/frontend/Main.java
@@ -1,12 +1,9 @@
 package com.maastricht.university.frontend;
 
-
-import com.maastricht.university.logic.game.GameState;
 import com.maastricht.university.logic.util.interfaces.IGameState;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,17 +15,16 @@ public class Main{
     int width = 800;
     int height = 500;
 
-    public ImageView p1 = new ImageView(new Image("/images/playerone.png"));
-    public ImageView p2 = new ImageView(new Image("/images/playertwo.png"));
-    public ImageView backView = new ImageView(new Image("/images/button_back.png"));
-    public ImageView exitView = new ImageView(new Image("/images/button_exit.png"));
-    public Image bambooBcg = new Image("/images/bamboo.jpeg", width, height, false, true);
-    public BackgroundImage bImg = new BackgroundImage(bambooBcg,
+    private ImageView p1 = new ImageView(new Image("/images/playerone.png"));
+    private ImageView p2 = new ImageView(new Image("/images/playertwo.png"));
+    private ImageView exitView = new ImageView(new Image("/images/button_exit.png"));
+    private Image bambooBcg = new Image("/images/bamboo.jpeg", width, height, false, true);
+    private BackgroundImage bImg = new BackgroundImage(bambooBcg,
             BackgroundRepeat.NO_REPEAT,
             BackgroundRepeat.NO_REPEAT,
             BackgroundPosition.DEFAULT,
             BackgroundSize.DEFAULT);
-    public Background bGround = new Background(bImg);
+    private Background bGround = new Background(bImg);
 
     public static Label p1Text = new Label(Integer.toString(p1groups()));
     public static Label p2Text = new Label(Integer.toString(p2groups()));
@@ -58,19 +54,12 @@ public class Main{
         p2.setFitHeight(width/2);
         playground.getChildren().add(p2);
 
-
         p2Text.setLayoutY(60.0);
         p2Text.setLayoutX(700.0);
         p2Text.setFont(new Font("Times New Roman", 25));
         p2Text.setTextFill(Color.WHITE);
         playground.getChildren().add(p2Text);
 
-        /*
-        Button p2Groups = new Button();
-        p2Groups.setLayoutY(65.0);
-        p2Groups.setLayoutX(700.0);
-        playground.getChildren().add(p2Groups);
-         */
 
         HoverableButton exit = new HoverableButton(50, height - height/12, 20, 20);
         exitView.setFitHeight(40);
@@ -91,13 +80,19 @@ public class Main{
         return scene;
     }
 
-
+    /**
+     * This method uses the GameState interface to return the number of groups for player 1
+     * @return number of groups for player 1
+     */
     public static int p1groups(){
         IGameState gameState = Factory.getGameState();
 
         return gameState.getTotalGroups(1);
     }
-
+    /**
+     * This method uses the GameState interface to return the number of groups for player 2
+     * @return number of groups for player 2
+     */
     public static int p2groups(){
         IGameState gameState = Factory.getGameState();
 
