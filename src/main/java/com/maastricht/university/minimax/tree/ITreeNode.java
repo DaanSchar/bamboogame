6884ed6b765897944d895646040cf8a0c60fe0bd
DaanSchar <Daan.Schaer@Gmail.com>
2021-10-24 16:29:31 +0200

    added interfaces

---
+++ src/main/java/com/maastricht/university/minimax/tree/ITreeNode.java
@@ -0,0 +1,72 @@
+package com.maastricht.university.minimax.tree;
+
+import java.util.List;
+
+/**
+ * Node for tree Data structure.
+ * there are no restrictions on how many children
+ * each node can have.
+ */
+public interface ITreeNode<E> {
+
+    /**
+     * @return Element stored by this node.
+     */
+    E getElement();
+
+    /**
+     * set the element which will be stored by this node
+     * @param element
+     */
+    void setElement(E element);
+
+    /**
+     * a parent is the predecessing node of this node.
+     * that means that the parent node stored this node as a child.
+     *
+     * a parent can have many children.
+     * a child has 1 parent.
+     *
+     * @return parent of this node.
+     */
+    ITreeNode<E> getParent();
+
+    /**
+     * a node can have many children nodes.
+     * @return list of children nodes
+     */
+    List<ITreeNode<E>> getChildren();
+
+    /**
+     * adds a child to node to this node.
+     * @param element
+     */
+    void addChild(E element);
+
+    /**
+     * compares all children to each other using the
+     * comparable interface.
+     *
+     * @return child with the largest element
+     */
+    ITreeNode<E> getMaxChild();
+
+    /**
+     * compares all children to each other using the
+     * comparable interface.
+     *
+     * @return child with the smallest element
+     */
+    ITreeNode<E> getMinChild();
+
+    /**
+     * @return boolean regarding if this node contains any children
+     */
+    boolean hasChildren();
+
+    /**
+     * deletes this node.
+     * node must have no children.
+     */
+    void delete();
+}
