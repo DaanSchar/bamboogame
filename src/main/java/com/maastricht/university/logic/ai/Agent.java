17878be2592aaf04d8be124d154f815860c00cab
DaanSchar <Daan.Schaer@Gmail.com>
2021-11-01 18:55:43 +0100

    edited agent

--- src/main/java/com/maastricht/university/logic/ai/Agent.java
+++ src/main/java/com/maastricht/university/logic/ai/Agent.java
@@ -8,20 +8,23 @@ public class Agent {
 
     private IGameState gameState;
     private int player;
-    private Random rand;
 
-    public Agent(IGameState gameState, int playerNumber) {
+    public Agent(IGameState gameState, final int playerNumber) {
         this.gameState = gameState;
         this.player = playerNumber;
-        rand = new Random();
     }
 
     public void move() {
         while (gameState.getPlayerTurn() == 2 && gameState.winner() == 0) {
-            int q = rand.nextInt(9);
-            int r = rand.nextInt(9);
-
-            gameState.move(q, r, player);
+            determineMove();
         }
+    }
+
+    private void determineMove() {
+        Random rand = new Random();
+        int q = rand.nextInt(9);
+        int r = rand.nextInt(9);
+
+        gameState.move(q, r, player);
     }
 }
