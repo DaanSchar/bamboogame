839c642937eaf620ba1b2312939745c9da0ad347
DaanSchar <Daan.Schaer@Gmail.com>
2021-11-09 13:42:13 +0100

    created iris neural network classifier

---
+++ src/main/java/com/maastricht/university/logic/ai/reinforcement/Iris.java
@@ -0,0 +1,152 @@
+package com.maastricht.university.logic.ai.reinforcement;
+
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.nd4j.evaluation.classification.Evaluation;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
+import org.nd4j.linalg.learning.config.Sgd;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
+
+public class Iris {
+
+    private static Logger log = LoggerFactory.getLogger(Iris.class);
+    private static String filename = "src/main/java/com/maastricht/university/logic/ai/reinforcement/iris.csv";
+
+    public static void main(String[] args) throws  Exception {
+        DataSet allData = convertCsvToDataSet();
+        allData.shuffle();
+
+        SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.65);  //Use 65% of data for training
+        DataSet trainingData = testAndTrain.getTrain();
+        DataSet testData = testAndTrain.getTest();
+
+        //We need to normalize our data. We'll use NormalizeStandardize (which gives us mean 0, unit variance):
+        DataNormalization normalizer = new NormalizerStandardize();
+        normalizer.fit(trainingData);           //Collect the statistics (mean/stdev) from the training data. This does not modify the input data
+        normalizer.transform(trainingData);     //Apply normalization to the training data
+        normalizer.transform(testData);         //Apply normalization to the test data. This is using statistics calculated from the *training* set
+
+        final int numInputs = 4; // number of features of the Iris data set
+        int outputNum = 3; // number of classes we want to predict
+        long seed = 6;
+
+        log.info("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .activation(Activation.TANH)
+                .weightInit(WeightInit.XAVIER)
+                .updater(new Sgd(0.1))
+                .l2(1e-4)
+                .list()
+                .layer(new DenseLayer.Builder().nIn(numInputs).nOut(3)
+                        .build())
+                .layer(new DenseLayer.Builder().nIn(3).nOut(3)
+                        .build())
+                .layer( new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .activation(Activation.SOFTMAX) //Override the global TANH activation with softmax for this layer
+                        .nIn(3).nOut(outputNum).build())
+                .build();
+
+        //run the model
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        //record score once every 100 iterations
+        model.setListeners(new ScoreIterationListener(100));
+
+        for (int i=0; i<2000; i++ ) {
+            model.fit(trainingData);
+        }
+
+        //evaluate the model on the test set
+        Evaluation eval = new Evaluation(3);
+        INDArray output = model.output(testData.getFeatures());
+        eval.eval(testData.getLabels(), output);
+        log.info(eval.stats());
+
+        ArrayList<DataSet> list = new ArrayList<>();
+        list.add(testData);
+        list.add(new DataSet(testData.getFeatures(), output));
+        plotDataset(list);
+    }
+
+    private static DataSet convertCsvToDataSet() throws Exception {
+        //First: get the dataset using the record reader. CSVRecordReader handles loading/parsing
+        int numLinesToSkip = 0;
+        char delimiter = ',';
+        RecordReader recordReader = new CSVRecordReader(numLinesToSkip,delimiter);
+        recordReader.initialize( new FileSplit( new File(filename) ) );
+
+        //Second: the RecordReaderDataSetIterator handles conversion to DataSet objects, ready for use in neural network
+        int labelIndex = 4;     //5 values in each row of the iris.txt CSV: 4 input features followed by an integer label (class) index. Labels are the 5th value (index 4) in each row
+        int numClasses = 3;     //3 classes (types of iris flowers) in the iris data set. Classes have integer values 0, 1 or 2
+        int batchSize = 150;    //Iris data set: 150 examples total. We are loading all of them into one DataSet (not recommended for large data sets)
+
+        DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader,batchSize,labelIndex,numClasses);
+        return iterator.next();
+    }
+
+    public static void plotDataset(ArrayList<DataSet> DataSetList) {
+
+        XYSeriesCollection c = new XYSeriesCollection();
+
+        int dscounter = 1; //use to name the dataseries
+        for (DataSet ds : DataSetList) {
+            INDArray features = ds.getFeatures();
+            INDArray outputs = ds.getLabels();
+
+            int nRows = features.rows();
+            XYSeries series = new XYSeries("S" + dscounter++);
+            for (int i = 0; i < nRows; i++) {
+                series.add(features.getRow(i).getDouble(0), outputs.getDouble(i));
+            }
+
+            c.addSeries(series);
+        }
+
+        String title = "Red = Predicted | Blue = Actual";
+        String xAxisLabel = "First Feature";
+        String yAxisLabel = "Class";
+        PlotOrientation orientation = PlotOrientation.VERTICAL;
+        boolean legend = false;
+        boolean tooltips = false;
+        boolean urls = false;
+        //noinspection ConstantConditions
+        JFreeChart chart = ChartFactory.createScatterPlot(title, xAxisLabel, yAxisLabel, c, orientation, legend, tooltips, urls);
+        JPanel panel = new ChartPanel(chart);
+
+        JFrame f = new JFrame();
+        f.add(panel);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.pack();
+        f.setTitle("Training Data");
+
+        f.setVisible(true);
+    }
+}
