14ec7cc2574edfde3cda15b6b02d5a90fed83508
DaanSchar <Daan.Schaer@Gmail.com>
2021-11-09 14:40:08 +0100

    copied GameObservationSpace

---
+++ src/main/java/com/maastricht/university/logic/ai/reinforcement/network/GameObservationSpace.java
@@ -0,0 +1,37 @@
+package com.maastricht.university.logic.ai.reinforcement.network;
+
+import org.deeplearning4j.rl4j.space.ObservationSpace;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+public class GameObservationSpace implements ObservationSpace<> {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int[] getShape() {
+        return new int[] { 1, Network.NUM_INPUTS };
+    }
+
+    @Override
+    public INDArray getLow() {
+        return Nd4j.create(createArray(Network.LOW_VALUE));
+    }
+
+    @Override
+    public INDArray getHigh() {
+        return Nd4j.create(createArray(Network.HIGH_VALUE));
+    }
+
+    private static double[] createArray(double value) {
+        double[] array = new double[Network.NUM_INPUTS];
+        for (int i = 0; i < array.length; i++)
+            array[i] = value;
+
+        return array;
+    }
+
+    }
+}
