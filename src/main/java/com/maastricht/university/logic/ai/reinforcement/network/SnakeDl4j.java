3604e726943a876d144e58f2ea094cb867c8228c
DaanSchar <Daan.Schaer@Gmail.com>
2021-11-11 23:41:52 +0100

    simplified rl training class

--- src/main/java/com/maastricht/university/logic/ai/reinforcement/network/SnakeDl4j.java
+++ src/main/java/com/maastricht/university/logic/ai/reinforcement/network/SnakeDl4j.java
@@ -1,44 +1,31 @@
 package com.maastricht.university.logic.ai.reinforcement.network;
 
 import com.maastricht.university.logic.ai.agent.Agent;
-import com.maastricht.university.logic.game.components.Hexagon;
-import com.maastricht.university.logic.game.components.LogicTile;
-import com.maastricht.university.logic.game.game.Move;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import com.maastricht.university.logic.ai.agent.ReinforcementAgent;
+import com.maastricht.university.logic.game.game.GameState;
 import org.deeplearning4j.rl4j.learning.sync.qlearning.discrete.QLearningDiscreteDense;
-import org.nd4j.linalg.api.ndarray.INDArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SnakeDl4j {
 
     private static final Logger LOG = LoggerFactory.getLogger(SnakeDl4j.class);
-    private com.maastricht.university.logic.game.game.GameState game = new com.maastricht.university.logic.game.game.GameState(4,2);
+    private GameState game = new GameState(4,2);
 
     public static void main(String[] args) {
         new SnakeDl4j();
     }
 
     public SnakeDl4j()  {
-//        new java.util.Timer().schedule(
-//                new java.util.TimerTask() {
-//                    @Override
-//                    public void run() {
-//                        train();
-//                    }
-//                },
-//                100
-//        );
 //        train();
-        evaluateNetwork("network-1636660087593.zip");
+//        evaluateNetwork("network-1636666442341.zip", new ReinforcementAgent(game, 2, "network-1636660087593.zip"));
+        evaluateNetwork("src/main/resources/networks/network-1636669584251.zip", new ReinforcementAgent(game, 2, "network-1636660087593.zip"));
     }
 
     private void train() {
-        String randomNetworkName = "network-" + System.currentTimeMillis() + ".zip";
+        String randomNetworkName = "src/main/resources/networks/network-" + System.currentTimeMillis() + ".zip";
 
         Environment mdp = new Environment();
         QLearningDiscreteDense<NeuralGameState> dql = new QLearningDiscreteDense<NeuralGameState>(
@@ -56,42 +43,32 @@ public class SnakeDl4j {
             LOG.error(e.getMessage(), e);
         }
 
-//        Factory.resetGameState();
-
-//            evaluateNetwork((com.maastricht.university.logic.game.game.GameState)Factory.getGameState(), randomNetworkName);
+        evaluateNetwork(randomNetworkName, new ReinforcementAgent(game, 2, "network-1636666442341.zip"));
     }
 
-    private void evaluateNetwork(String randomNetworkName) {
-         MultiLayerNetwork multiLayerNetwork = Network.loadNetwork(randomNetworkName);
-        System.out.println("Starting evaluation");
-        int highscore = 0;
+    /**
+     * Evaluates the network on how good it is compared a different agent.
+     */
+    private void evaluateNetwork(String randomNetworkName, Agent opponentAgent) {
         int totalScore = 0;
-        for (int i = 0; i < 200; i++) {
+        int totalGames = 200;
+
+        Agent RLAgent = new ReinforcementAgent(game, 1, randomNetworkName);
+//        opponentAgent = new Agent(game, 2);
+
+        for (int i = 0; i < totalGames; i++) {
             int score = 0;
+
             while (!game.isGameOver()) {
                 try {
-                     NeuralGameState state = new NeuralGameState(playerColorVector(game));
-                     INDArray output = multiLayerNetwork.output(state.getMatrix(), false);
-                    double[] data = output.data().asDouble();
+                    RLAgent.move();
+                    opponentAgent.move();
 
-                    int maxValueIndex = 0;
+                    if (game.winner() == 1)
+                        score = 100;
+                    else if (game.winner() == 2)
+                        score = -100;
 
-                    for (int j = 0; j < data.length; j++)
-                        if (data[j] > data[maxValueIndex] )
-                            maxValueIndex = j;
-
-                    List<Move> moveList = removeIllegalMoves(((Hexagon<LogicTile>)game.getBoard().getTileMap()).moveVector());
-                    Move move = moveList.get(maxValueIndex * moveList.size() / 61);
-                    game.move(move.getX(), move.getY(), 1);
-//                    WindowUpdater.update(game);
-                    Agent agent = new Agent(game, 2);
-                    agent.move();
-//                    WindowUpdater.update(game);
-                    score = !game.isGameOver() ? 0 : game.winner() == 1 ? 100 : -100;
-
-
-                    // Needed so that we can see easier what is the game doing
-                    Network.pause(1);
                 } catch (final Exception e) {
                     LOG.error(e.getMessage(), e);
                     Thread.currentThread().interrupt();
@@ -100,35 +77,23 @@ public class SnakeDl4j {
 
             LOG.info("Score of iteration '{}' was '{}'", i, score);
             totalScore+=score;
-            if (score > highscore) {
-                highscore = score;
-            }
 
-            // Reset the game
-//            Factory.resetGameState();
-            game = new com.maastricht.university.logic.game.game.GameState(4, 2);
+            game = new GameState(4, 2);
+            RLAgent.setGameState(game);
+            opponentAgent.setGameState(game);
         }
-        LOG.info("Finished evaluation of the network, average score was '{}'", totalScore/2000);
+        LOG.info("Finished evaluation of the network, average score was '{}'", totalScore/totalGames);
     }
 
-
-    public List<Move> removeIllegalMoves(List<Move> moveList) {
-
-        List<Move> legalList = new ArrayList<>();
-        for (int i = 0; i < moveList.size(); i++)
-            if (game.isLegal(moveList.get(i).getX(), moveList.get(i).getY(), 1))
-                legalList.add(moveList.get(i));
-
-        return legalList;
-    }
-
-    private double[] playerColorVector(com.maastricht.university.logic.game.game.GameState game) {
-        List<LogicTile> logicVector = game.getBoard().getTileMap().vector();
-        double[] vector = new double[logicVector.size()];
-
-        for (int i = 0; i < logicVector.size(); i++)
-            vector[i] = logicVector.get(i).getPlayerColor();
-
-        return vector;
+    private void trainWithJavaFX() {
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        train();
+                    }
+                },
+                100
+        );
     }
 }
