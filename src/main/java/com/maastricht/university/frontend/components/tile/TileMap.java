b53a538479dff216708a9bc51046feb6b4101a93
DaanSchar <Daan.Schaer@Gmail.com>
2021-12-08 23:16:05 +0100

    cleaned up TileMap class

--- src/main/java/com/maastricht/university/frontend/components/tile/TileMap.java
+++ src/main/java/com/maastricht/university/frontend/components/tile/TileMap.java
@@ -6,33 +6,24 @@ import javafx.scene.layout.Pane;
 
 public class TileMap {
 
-    private int dimension = 9;
+    private int dimension;
     private final Hexagon<Tile> hexagon;
-
-    private int mapX;
-    private int mapY;
-    private int TileSize;
-    private Pane tileMapPane;
 
     public TileMap(int dimension, int mapX, int mapY, int TileSize) {
         this.dimension = dimension;
-        this.mapX = mapX;
-        this.mapY = mapY;
-        this.TileSize = TileSize;
-
         hexagon = new Hexagon<>(dimension/2);
 
         for (int i = 0; i < hexagon.size(); i++)
             for (int j = 0; j < hexagon.size(); j++)
                 hexagon.insert(i, j,
                         new Tile(i, j,
-                                mapX + (i * TileSize) + (i + j * TileSize / 2),
+                                mapX + (i * TileSize) + (i + j * TileSize / 2f),
                                 mapY + j * TileSize, 30)
                 );
     }
 
     public Pane getTileMapPane() {
-        tileMapPane = new Pane();
+        Pane tileMapPane = new Pane();
 
         for(int i = 0; i<dimension;i++) {
             for(int j = 0; j<dimension;j++) {
