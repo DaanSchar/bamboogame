e8f6709cafcddb5035850e48edcd5e314b26c21e
DaanSchar <Daan.Schaer@Gmail.com>
2021-12-08 23:06:27 +0100

    Added new Main class

---
+++ src/main/java/com/maastricht/university/frontend/Main.java
@@ -0,0 +1,13 @@
+package com.maastricht.university.frontend;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setScene(Factory.getMenuScene());
+        stage.show();
+    }
+}
