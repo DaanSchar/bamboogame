8885ecf92a68875230cab0f5b8c4ab86a63225ee
michellefilipaa <m.lear@student.maastrichtuniversity.nl>
2022-01-06 13:30:20 +0100

    code cleanup for agent2

--- src/main/java/com/maastricht/university/logic/ai/agent/Agent2.java
+++ src/main/java/com/maastricht/university/logic/ai/agent/Agent2.java
@@ -2,14 +2,11 @@ package com.maastricht.university.logic.ai.agent;
 
 import com.maastricht.university.logic.ai.hybrid.ComputeLeafNodes;
 import com.maastricht.university.logic.ai.hybrid.Search;
-import com.maastricht.university.logic.ai.minimax.tree.ITree;
 import com.maastricht.university.logic.ai.minimax.tree.ITreeNode;
-import com.maastricht.university.logic.game.components.Board;
 import com.maastricht.university.logic.game.game.Move;
 import com.maastricht.university.logic.game.util.interfaces.IGameState;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+
 
 public class Agent2 implements IAgent{
 
@@ -59,50 +56,6 @@ public class Agent2 implements IAgent{
      */
     public void setGameState(IGameState gameState) {
         this.gameState = gameState;
-    }
-
-    /**
-     * Make a random legal move
-     */
-    private void determineRandomMove() {
-        Random rand = new Random();
-        ArrayList<Move> moveList = gameState.getLegalMoves(player);
-        int index = rand.nextInt(moveList.size());
-        Move move = moveList.get(index);
-
-        System.out.println("Move: (" +move.getX()+ ", " +move.getY()+ ", " +player+ ")");
-        gameState.move(move.getX(), move.getY(), player);
-    }
-
-    /**
-     * Make a move that doesn't join groups together (if possible), otherwise random
-     */
-    private void determineMove() {
-        Board board = gameState.getBoard();
-
-        Random rand = new Random();
-        ArrayList<Move> moveList = gameState.getLegalMoves(player);
-        ArrayList<Move> betterMoves = new ArrayList<>();
-
-        //If none of the moves has neighbouring groups of equal color its a non-connecting move
-        // hopefully better
-        for(Move m : moveList) {
-            if(board.getNeighboringGroups(m.getX(), m.getY(), player).size() == 0)
-                betterMoves.add(m);
-        }
-
-        if(betterMoves.size() != 0) {
-            int index = rand.nextInt(betterMoves.size());
-            Move move = betterMoves.get(index);
-            gameState.move(move.getX(), move.getY(), player);
-            System.out.println("Move: (" + move.getX() + ", " + move.getY() + ", " + player + ")");
-        }
-        else {
-            int index = rand.nextInt(moveList.size());
-            Move move = moveList.get(index);
-            gameState.move(move.getX(), move.getY(), player);
-            System.out.println("Move: (" + move.getX() + ", " + move.getY() + ", " + player + ")");
-        }
     }
 
     /**
