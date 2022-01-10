b3ff353cb2141fb16d7cd9a35da006c41f84349f
michellefilipaa <m.lear@student.maastrichtuniversity.nl>
2022-01-10 14:15:44 +0100

    some training

--- src/main/java/com/maastricht/university/logic/ai/reinforcement/network/Network.java
+++ src/main/java/com/maastricht/university/logic/ai/reinforcement/network/Network.java
@@ -19,7 +19,7 @@ public class Network {
     public static final double HIGH_VALUE = 2;
 
     private static final int stepsPerEpoch = 29;
-    private static final int maxEpochs = 500;
+    private static final int maxEpochs = 2000;
 
     public static QLearningConfiguration buildConfig() {
         return QLearningConfiguration.builder()
